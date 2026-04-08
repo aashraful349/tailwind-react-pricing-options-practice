a35e8545ec83038601849a13e647f6fb167e3263
aashraful349 <aashraful349@gmail.com>
2026-04-09 00:48:51 +0600

    learned charts

--- src/Components/MarksChart/MarksChart.jsx
+++ src/Components/MarksChart/MarksChart.jsx
@@ -1,4 +1,6 @@
+import { X } from 'lucide-react';
 import React, { use } from 'react';
+import { Bar, BarChart, XAxis, YAxis } from 'recharts';
 
 const MarksChart = ({ marksPromise }) => {
     const marksDataRes=use(marksPromise);
@@ -6,7 +8,13 @@ const MarksChart = ({ marksPromise }) => {
     console.log(marksData);
     return (
         <div>
-            
+            <BarChart width={500} height={300} data={marksData}>
+                <XAxis dataKey={"name"}></XAxis>
+                <YAxis></YAxis>
+                <Bar dataKey="marks.math" fill='blue'></Bar>
+                <Bar dataKey="marks.physics" fill='red'></Bar>
+                <Bar dataKey="marks.chemistry" fill='green'></Bar>
+            </BarChart>
         </div>
     );
 };
